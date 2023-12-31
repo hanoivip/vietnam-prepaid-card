7d5d2cae86869321cd3e4adcf81e4cf65b6618b0
unknown <game.oh.vn@gmail.com>
2023-12-31 10:31:30 +0700

    f

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 // Webtopup migration
-Route::middleware('auth:api')->prefix('api')->namespace('Hanoivip\Payment\Controllers')->group(function () {
+Route::middleware('auth:api')->prefix('api')->namespace('Hanoivip\VietnamPrepaidCard\Controllers')->group(function () {
 	// keep path :(
     Route::any('/webtopup', 'CardToWebFlow@index');
-	Route::any('/pay/quick/topup', 'WebTopup@quickTopup');
-	//Route::any('/pay/quick/recharge', 'WebTopup@quickRecharge');
 });
